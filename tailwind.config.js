4c6c7dc66552920525695ae5435d49028a471643
Leonardo Alcantara <leonardo.alcantara@gmail.com>
2022-02-18 17:52:05 -0300

    Instalando o tailwind e ajustando os arquivos básicos de configuração

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  content: [
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/*.html',
+    './**/**/*.html',
+    './**/*.html',
+    './*.{html,js}'
+  ],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'black': '#000000',
+      'texto': '#F7F6F6',
+      'verde': '#28E655',
+      'cinza': '#777',
+      'facebook': '#3B5998',
+      'youtube': '#CD201F',
+      'instagram': '#262626',
+    },
+    fontFamily: {
+      sans:   ['Roboto', 'sans-serif'],
+      display: ['Raleway', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
